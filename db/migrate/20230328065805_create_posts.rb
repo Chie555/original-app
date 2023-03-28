6c8eb8d4013083dd868483fce3ca6d29087ccc98
Chie555 <adgjmp159nat@gmail.com>
2023-03-28 17:41:04 +0900

    Basic認証修正

---
+++ db/migrate/20230328065805_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :weight,          null: false, default: ""
+      t.string :fat_percentage,    default: ""
+      t.integer :today1_id
+      t.string :today1_memo
+      t.integer :today2_id
+      t.string :today2_memo
+      t.integer :today3_id
+      t.string :today3_memo
+      t.text :memo
+      t.timestamps
+    end
+  end
+end
