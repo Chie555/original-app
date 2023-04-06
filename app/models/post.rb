686d1a6893111356f5eb382ff53be2c038bab105
Chie555 <adgjmp159nat@gmail.com>
2023-04-06 11:33:26 +0900

    デプロイ用修正4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-  validates :weight, presence: true, numericality: true, { greater_than_or_equal_to: 30.0, less_than_or_equal_to: 300.0, message: 'は半角数値の30.0kg〜300.0kgの範囲内で入力してください' }
-  validates :fat_percentage, numericality: true, { greater_than_or_equal_to: 3.0, less_than_or_equal_to: 99.9, message: 'は半角数値の3.0%〜99.9%の範囲内で入力してください' }
-  validates :today1_id, presence: {if: '!today1_memo.blank?' }, numericality: { other_than: 0, message: 'を選択してください' }
-  validates :today2_id, presence: {if: '!today2_memo.blank?' }, numericality: { other_than: 0, message: 'を選択してください' }
-  validates :today3_id, presence: {if: '!today3_memo.blank?' }, numericality: { other_than: 0, message: 'を選択してください' }
+  validates :weight, presence: true, numericality: true
+  validates :fat_percentage, numericality: true
+  validates :today1_id, numericality: { other_than: 0, message: 'を選択してください' }
+  validates :today2_id, numericality: { other_than: 0, message: 'を選択してください' }
+  validates :today3_id, numericality: { other_than: 0, message: 'を選択してください' }
   validates :image, presence: true
   # アソシエーション
   belongs_to :user
