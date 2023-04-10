8d6d02df1ea8f93a4aad0762acab301bfe3a5558
Chie555 <adgjmp159nat@gmail.com>
2023-04-10 23:32:55 +0900

    ユーザー登録画面

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  # 半角英数字混合
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
+  # 生年月日
+  validates :birthday, presence: true
+  # アソシエーション
+  has_many :posts
+  # has_many :follows
+  # has_many :comments
+end
